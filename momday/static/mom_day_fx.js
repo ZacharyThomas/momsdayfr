c083abe28979272d48276287e30fe87333eb7623
Zack_Smith <zack.smith@temple.edu>
2015-05-30 22:49:06 -0400

    Added slideshow on first rose.

---
+++ momday/static/mom_day_fx.js
@@ -0,0 +1,15 @@
+function Slideshow(){
+    alert("Called Slideshow function");
+};
+
+function Vidzzz(){
+    alert("Called Video funtion");
+};
+
+function Poem(){
+    alert("Called Poem function");
+};
+
+function Stories(){
+    alert("Called Stories function");
+};
